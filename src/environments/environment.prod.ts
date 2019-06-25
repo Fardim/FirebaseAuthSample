9ee95feb78e119c264e44cb4e60628dcbced83a9
Fardim <fardim_drmc_9094@yahoo.com>
2019-06-25 18:35:03 +0600

    Initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCRMU__jf0g-DKYZC-SrcvoOMXI0l6RAKU",
+    authDomain: "authprovider-5a34d.firebaseapp.com",
+    databaseURL: "https://authprovider-5a34d.firebaseio.com",
+    projectId: "authprovider-5a34d",
+    storageBucket: "",
+    messagingSenderId: "147569349803",
+    appId: "1:147569349803:web:fa80b6165e5f254d"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCRMU__jf0g-DKYZC-SrcvoOMXI0l6RAKU",
+    authDomain: "authprovider-5a34d.firebaseapp.com",
+    databaseURL: "https://authprovider-5a34d.firebaseio.com",
+    projectId: "authprovider-5a34d",
+    storageBucket: "",
+    messagingSenderId: "147569349803",
+    appId: "1:147569349803:web:fa80b6165e5f254d"
+  }
 };
 
 /*
